bcfc5eeed47a2710e62e09945a009bf4113462c3
brindharamesh28 <82710126+brindharamesh28@users.noreply.github.com>
2021-04-22 01:04:59 +0530

    Add files via upload

---
+++ projects/multiselect28/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of multiselect28
+ */
+
+export * from './lib/multiselect28.service';
+export * from './lib/multiselect28.component';
+export * from './lib/multiselect28.module';
