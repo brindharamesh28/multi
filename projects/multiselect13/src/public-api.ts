d1784933fda31ca3406cb8441d0857393ceb7539
brindharamesh28 <82710126+brindharamesh28@users.noreply.github.com>
2021-04-22 01:04:21 +0530

    Delete projects/multiselect13 directory

--- projects/multiselect13/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of multiselect13
- */
-
-export * from './lib/multiselect13.service';
-export * from './lib/multiselect13.component';
-export * from './lib/multiselect13.module';
