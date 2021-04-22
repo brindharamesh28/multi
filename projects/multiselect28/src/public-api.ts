bbe9b1e5e75c9afdad9bd174eeca0bf86bd82b77
brindharamesh28 <82710126+brindharamesh28@users.noreply.github.com>
2021-04-22 13:49:19 +0530

    Delete projects/multiselect28 directory

--- projects/multiselect28/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of multiselect28
- */
-
-export * from './lib/multiselect28.service';
-export * from './lib/multiselect28.component';
-export * from './lib/multiselect28.module';
